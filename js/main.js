1655a18959355a8fd41904774b940b6d9fa6b17e
carmensiay0316 <csia0019@student.monash.edu>
2026-05-30 23:00:38 +1000

    annotation, colour,  layout fixed

--- js/main.js
+++ js/main.js
@@ -63,10 +63,6 @@ vegaEmbed("#sector_area_2019_2025", "vega/sector_area_2019to2025.json", { action
       }
     }
 
-    result.view.addEventListener("mouseover", function(event, item) {
-      updateSectorAnnotation(item);
-    });
-
     result.view.addEventListener("click", function(event, item) {
       updateSectorAnnotation(item);
     });
@@ -81,16 +77,168 @@ vegaEmbed("#sector_area_2019_2025", "vega/sector_area_2019to2025.json", { action
   .catch(console.error);
 
 vegaEmbed("#sector_bump_2019_2025", "vega/sector_bump_2019to2025.json", { actions: false })
+  .then(function(result) {
+    function updateBumpAnnotation(item) {
+      if (item && item.datum && item.datum.sector_label && item.datum.year) {
+        result.view
+          .signal("selected_bump_sector", item.datum.sector_label)
+          .signal("selected_bump_year", item.datum.year)
+          .runAsync();
+      }
+    }
+
+    result.view.addEventListener("click", function(event, item) {
+      updateBumpAnnotation(item);
+    });
+
+    result.view.addEventListener("dblclick", function() {
+      result.view
+        .signal("selected_bump_sector", "")
+        .signal("selected_bump_year", 0)
+        .runAsync();
+    });
+  })
   .catch(console.error);
 
 vegaEmbed("#source_region_waffle_2025", "vega/source_region_waffle_2025.json", { actions: false })
+  .then(function(result) {
+    function updateWaffleAnnotation(item) {
+      if (item && item.datum && item.datum.region) {
+        result.view
+          .signal("selected_waffle_region", item.datum.region)
+          .runAsync();
+      }
+    }
+
+    result.view.addEventListener("click", function(event, item) {
+      updateWaffleAnnotation(item);
+    });
+
+    result.view.addEventListener("dblclick", function() {
+      result.view
+        .signal("selected_waffle_region", "")
+        .runAsync();
+    });
+  })
   .catch(console.error);
 
-vegaEmbed("#state_income_indexed_line", "vega/state_income_indexed_line.json", { actions: false })
+vegaEmbed("#state_income_indexed_line", "vega/state_income_indexed_line.json", {
+  actions: false
+})
+  .then(function(result) {
+    function updateIncomeAnnotation(item) {
+      if (item && item.datum && item.datum.state_name_full && item.datum.financial_year) {
+        result.view
+          .signal("selected_income_state", item.datum.state_name_full)
+          .signal("selected_income_year", item.datum.financial_year)
+          .runAsync();
+      }
+    }
+
+    result.view.addEventListener("click", function(event, item) {
+      updateIncomeAnnotation(item);
+    });
+
+    result.view.addEventListener("dblclick", function() {
+      result.view
+        .signal("selected_income_state", "")
+        .signal("selected_income_year", "")
+        .runAsync();
+    });
+  })
   .catch(console.error);
 
-vegaEmbed("#sector_income_heatmap", "vega/sector_income_heatmap.json", { actions: false })
+vegaEmbed("#sector_income_heatmap", "vega/sector_income_heatmap.json", {
+  actions: false
+})
+  .then(function(result) {
+    function updateHeatmapAnnotation(item) {
+      if (item && item.datum && item.datum.sector_label && item.datum.financial_year) {
+        result.view
+          .signal("selected_heatmap_sector", item.datum.sector_label)
+          .signal("selected_heatmap_year", item.datum.financial_year)
+          .runAsync();
+      }
+    }
+
+    result.view.addEventListener("click", function(event, item) {
+      updateHeatmapAnnotation(item);
+    });
+
+    result.view.addEventListener("dblclick", function() {
+      result.view
+        .signal("selected_heatmap_sector", "")
+        .signal("selected_heatmap_year", "")
+        .runAsync();
+    });
+  })
   .catch(console.error);
 
-vegaEmbed("#student_net_migration_gap_map", "vega/student_net_migration_map.json", { actions: false })
+vegaEmbed("#student_net_migration_gap_map", "vega/student_net_migration_map.json", {
+  actions: false
+})
+  .then(function(result) {
+    const panel = document.getElementById("migration_annotation_panel");
+
+    function formatNumber(value) {
+      const num = Number(String(value).replace(/,/g, ""));
+      if (Number.isNaN(num)) {
+        return value;
+      }
+      return num.toLocaleString("en-AU");
+    }
+
+    function formatSignedNumber(value) {
+      const num = Number(String(value).replace(/,/g, ""));
+      if (Number.isNaN(num)) {
+        return value;
+      }
+      return (num > 0 ? "+" : "") + num.toLocaleString("en-AU");
+    }
+
+    function balanceText(value) {
+      const num = Number(String(value).replace(/,/g, ""));
+      if (num > 0) {
+        return "Net student gain";
+      }
+      if (num < 0) {
+        return "Net student loss";
+      }
+      return "Balanced flow";
+    }
+
+    function resetMigrationPanel() {
+      panel.innerHTML = `
+        <h4>Selected state details</h4>
+        <p>Click a state on the map to show migration details.</p>
+      `;
+    }
+
+    function updateMigrationAnnotation(item) {
+      if (!item || !item.datum || !item.datum.state_name) {
+        return;
+      }
+
+      const d = item.datum;
+
+      panel.innerHTML = `
+        <h4>Selected state details</h4>
+
+        <p><strong>State:</strong><br>${d.state_name}</p>
+        <p><strong>Financial year:</strong> ${d.financial_year}</p>
+        <p><strong>Student arrivals:</strong> ${formatNumber(d.student_arrivals)}</p>
+        <p><strong>Student departures:</strong> ${formatNumber(d.student_departures)}</p>
+        <p><strong>Net migration:</strong> ${formatSignedNumber(d.net_student_migration)}</p>
+        <p><strong>Balance:</strong> ${balanceText(d.net_student_migration)}</p>
+      `;
+    }
+
+    result.view.addEventListener("click", function(event, item) {
+      updateMigrationAnnotation(item);
+    });
+
+    result.view.addEventListener("dblclick", function() {
+      resetMigrationPanel();
+    });
+  })
   .catch(console.error);
